33038670012ca6cff46517762a4312798978993d
Leszek Grzanka <grzanka@agh.edu.pl>
2021-12-18 22:38:18 +0100

    hltHighLevel whole module import (following PR review)

--- Calibration/PPSAlCaRecoProducer/python/PPSAlCaRecoProducer_cff.py
+++ Calibration/PPSAlCaRecoProducer/python/PPSAlCaRecoProducer_cff.py
@@ -1,14 +1,15 @@
 import FWCore.ParameterSet.Config as cms
 
-import copy
-from HLTrigger.HLTfilters.hltHighLevel_cfi import *
-hltFilter_express = copy.deepcopy(hltHighLevel)
-hltFilter_express.TriggerResultsTag = cms.InputTag("TriggerResults","","HLTX")
-hltFilter_express.HLTPaths = ['HLT_PPSMaxTracksPerArm1_v1']
+import HLTrigger.HLTfilters.hltHighLevel_cfi as hlt
+hltFilter_express = hlt.hltHighLevel.clone(
+  TriggerResultsTag = cms.InputTag("TriggerResults","","HLTX"),
+  HLTPaths = ['HLT_PPSMaxTracksPerArm1_v1']
+)
 
-hltFilter_prompt = copy.deepcopy(hltHighLevel)
-hltFilter_prompt.TriggerResultsTag = cms.InputTag("TriggerResults","","HLTX")
-hltFilter_prompt.HLTPaths = ['HLT_PPSMaxTracksPerRP4_v1']
+hltFilter_prompt = hlt.hltHighLevel.clone(
+  TriggerResultsTag = cms.InputTag("TriggerResults","","HLTX"),
+  HLTPaths = ['HLT_PPSMaxTracksPerRP4_v1']
+)
 
 from EventFilter.CTPPSRawToDigi.ctppsRawToDigi_cff import *
 from RecoPPS.Configuration.recoCTPPS_cff import *
